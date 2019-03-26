8d55e4693ee8e18e13adb7d941eb8ea8d295c1a1
Cosmin Potocean <cosmycx@gmail.com>
2019-03-26 14:40:37 -0400

    port change 8080

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 
 	s.router.HandleFunc("/", home)
 
-	log.Println("Starting server on port :4000")
-	log.Fatal(http.ListenAndServe(":4000", s.router))
+	log.Println("Starting server on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", s.router))
 
 } // .main
 
